ad5cf03309a5657366ca0681ed3aa31252a26230
nagatapote <hiroaki29p@icloud.com>
2022-06-12 17:26:02 +0900

    add: graphql導入

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { TodoModule } from './todo/todo.module';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      // schemaファイルのパスを指定
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      // 生成されたschemaを自動でsortされるためのオプションをオンにする
+      sortSchema: true,
+    }),
+    TodoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
